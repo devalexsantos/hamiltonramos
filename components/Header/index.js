73e251738e191010fc162cd770a66a1f5d03543a
devalexsantos <devalexsantos@gmail.com>
2021-10-13 23:31:38 -0300

    aplicando alteraçoes de estilo

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
     <Container>
       <Area>
       <Logo>
-        <img src="/images/logo_hamilton.png"/>
+        <img src="/images/logo_hamilton.png" alt="Logo Hamilton Ramos"/>
       </Logo>
       <ProfileBusiness>
       <h1><strong>HR Advocacia</strong> é um escritório que sempre busca um atendimento personalizado e acertivo ao cliente, com o objetivo primordial em assegurar um serviço transparente, criativo & Eficaz.</h1>
