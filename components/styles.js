6c57f6b18b8564dafd22d9ba7b5c615e29970ebd
devalexsantos <devalexsantos@gmail.com>
2021-10-15 13:59:26 -0300

    ajustando opacidade fundo

--- components/styles.js
+++ components/styles.js
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-background-image: url("images/background-2.jpg");
+background-image: url("images/background-desk-50.jpg");
 background-color: rgb(10, 24, 63);
 background-size: cover;
 background-repeat: no-repeat;
 background-attachment: fixed;
 width: 100vw;
 height: 100%;
+
+@media(max-width: 400px) {
+  background-image: url("images/background-mobile-50.jpg");
+  }
 `;
 
 export const Area = styled.div`
